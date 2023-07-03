b5e078cc133a0a7a20eaa1dafc0360859e7660c0
ghsantos <ghsantos933@gmail.com>
2023-07-03 18:31:00 -0300

    Media Player: add option to change socket url

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,18 +1,40 @@
 import React, { useState, useEffect } from 'react'
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  TouchableOpacity,
+} from 'react-native'
 import Icon from 'react-native-vector-icons/Ionicons'
 import io from 'socket.io-client'
+import SystemNavigationBar from 'react-native-system-navigation-bar'
 
-const socket = io('http://192.168.31.248:3000')
+const PORT = '3000'
+const URL = 'http://192.168.31.75'
+
+const socket = io(`${URL}:${PORT}`)
 
 function Player(): JSX.Element {
   const [isConnected, setIsConnected] = useState(socket.connected)
+  const [isEditing, setIsEditing] = useState(false)
+  const [url, setUrl] = useState(URL)
+
   const [trackState, setTrackState] = useState({
     isPlaying: false,
     artist: '',
     trackTitle: '',
     albumTitle: '',
   })
+
+  const onReconnectUrl = () => {
+    setIsEditing(false)
+    SystemNavigationBar.navigationHide()
+
+    socket.io.uri = `${url}:${PORT}`
+
+    socket.disconnect().connect()
+  }
 
   useEffect(() => {
     socket.on('connect', () => {
@@ -34,23 +56,53 @@ function Player(): JSX.Element {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>{trackState.trackTitle}</Text>
-      <View style={styles.controls}>
-        <TouchableOpacity onPress={() => socket.emit('prev', '')}>
-          <Icon name="play-skip-back-outline" size={54} color="#eee" />
-        </TouchableOpacity>
+      {isConnected && (
+        <>
+          <Text style={styles.title} numberOfLines={1}>
+            {trackState.trackTitle}
+          </Text>
+          <View style={styles.controls}>
+            <TouchableOpacity onPress={() => socket.emit('prev', '')}>
+              <Icon name="play-skip-back-outline" size={54} color="#eee" />
+            </TouchableOpacity>
 
-        <TouchableOpacity onPress={() => socket.emit('playpause', '')}>
-          {trackState.isPlaying ? (
-            <Icon name="pause-outline" size={54} color="#eee" />
+            <TouchableOpacity onPress={() => socket.emit('playpause', '')}>
+              {trackState.isPlaying ? (
+                <Icon name="pause-outline" size={54} color="#eee" />
+              ) : (
+                <Icon name="play-outline" size={54} color="#eee" />
+              )}
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => socket.emit('next', '')}>
+              <Icon name="play-skip-forward-outline" size={54} color="#eee" />
+            </TouchableOpacity>
+          </View>
+        </>
+      )}
+
+      {!isConnected && (
+        <>
+          {isEditing ? (
+            <TextInput
+              value={url}
+              onChangeText={setUrl}
+              style={styles.input}
+              onSubmitEditing={onReconnectUrl}
+            />
           ) : (
-            <Icon name="play-outline" size={54} color="#eee" />
+            <Text style={styles.title}>No connection available</Text>
           )}
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => socket.emit('next', '')}>
-          <Icon name="play-skip-forward-outline" size={54} color="#eee" />
-        </TouchableOpacity>
-      </View>
+
+          <View style={styles.controls}>
+            <TouchableOpacity onPress={() => setIsEditing(!isEditing)}>
+              <Icon name="ios-create-outline" size={40} color="#eee" />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => socket.connect()}>
+              <Icon name="ios-refresh" size={40} color="#eee" />
+            </TouchableOpacity>
+          </View>
+        </>
+      )}
     </View>
   )
 }
@@ -74,6 +126,15 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-around',
     paddingHorizontal: 14,
+  },
+  input: {
+    color: '#000',
+    backgroundColor: '#ddd',
+    fontSize: 12,
+    width: '74%',
+    height: 38,
+    borderRadius: 4,
+    marginBottom: 8,
   },
 })
 
